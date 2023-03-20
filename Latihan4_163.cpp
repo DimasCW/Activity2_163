e6a66bc9afa1df5a5aa64f140c8742eb1d721517
DimasCW <cdimas292@gmail.com>
2023-03-20 14:49:36 +0700

    menambahkan float

--- Latihan4_163.cpp
+++ Latihan4_163.cpp
@@ -3,6 +3,13 @@
 
 #include <iostream>
 
+float LuasPersegi(float p, float l) {
+    return p * l;
+}
+float LuasLingkaran(float r) {
+    return 3.14 * r * r;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
